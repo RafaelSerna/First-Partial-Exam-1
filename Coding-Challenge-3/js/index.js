63547f70f87ddcf428131da32847d1b6b93def65
Rafael Serna <rafaelserna@Rafaels-MacBook-Pro.local>
2020-03-27 08:21:42 -0600

    Last challenge

--- Coding-Challenge-3/js/index.js
+++ Coding-Challenge-3/js/index.js
@@ -1,3 +1,16 @@
-document.addEventListener("submitBtn", function() {
-	document.getElementById("fizzBuzzNumber").innerHTML
+document.addEventListener("click", function() {
+	var name;
+	var num = document.getElementById("fizzBuzzNumber");
+	for (let i = 0; i < num; i++) {
+		if (i % 3 === 0 && i % 5 === 0) {
+			name = "fizzbuzz"
+		}
+		else if (i % 3 === 0) {
+			name = "fizz"
+		}
+		else if (i % 5 === 0) {
+			name = "buzz"
+		}
+		document.getElementById("box").innerHTML = name;
+	}
 });
